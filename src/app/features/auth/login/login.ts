7d80220f56698924351d7e7c5ac3bbb8bbd1994e
Merna Elnashar <merrna.hamada@gmail.com>
2026-05-05 23:28:05 +0300

    feat: redesign login and registration pages with new cohesive branding and enhanced form styling

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -3,12 +3,11 @@ import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { ZoraInputComponent } from '../../../shared/components/zora-input/zora-input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink,ZoraInputComponent],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
